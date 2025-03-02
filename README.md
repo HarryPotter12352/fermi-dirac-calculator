a585f6f499b19d809e5df43eae3eb0b505164779
HarryPotter12352 <harrypottr12352.github@outlook.com>
2025-03-02 18:48:46 +0530

    Added gamma function using numerical integration

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fermi-dirac-calculator
+
 A script written in C++ to compute the various Fermi-Dirac quantities for a given species.
+
+While writing these scripts, I try to use my own functions as much as I can, all the way to basic numerical integration. I resort to using the MATLAB engine for symbolic integration.
--- math_helpers.cpp
+++ math_helpers.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <complex>
 
-
 std::complex<double> polylog(std::complex<double> s, std::complex<double> z){
     std::complex<double> result(0,0);
     int n = 1;
@@ -21,4 +20,25 @@ std::complex<double> polylog(std::complex<double> s, std::complex<double> z){
         }
         return result;
     }
-} 
+}
+
+
+std::complex<double> gamma_integrand(std::complex<double> t, std::complex<double> z){
+    if (std::abs(t) < 1e-10){
+        std::cout << "Invalid value of t. Terminating computation";
+        return std::complex<double>(0,0);
+    }
+    return std::pow(t,z-1.0)*std::exp(-t);
+}
+
+std::complex<double> gamma(std::complex<double> z, double lower_limit = 0, double upper_limit = 1e5, double steps = 1e-5){
+    double step_size = (upper_limit - lower_limit)/steps;
+    std::complex<double> integral(0,0);
+    integral += 0.5 *(gamma_integrand(lower_limit, z) + gamma_integrand(upper_limit, z));
+
+    for (int i = 1; i < steps; i++){
+        double t = lower_limit + i * step_size;
+        integral+= gamma_integrand(t,z);
+    }
+    return integral;
+}
